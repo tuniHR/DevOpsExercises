0bf1a80cdb480507063384f3477a29f8c541572e
Henri Rantala <henri.r.rantala@tuni.fi>
2024-11-08 10:50:21 +0200

    nginx exercise

--- service2/service2.js
+++ service2/service2.js
@@ -1,7 +1,10 @@
 const http = require('http');
-const { execSync } = require('child_process');
+const { exec, execSync } = require('child_process');
+
 
 const PORT = 8200;
+
+
 
 const requestListener = (req, res) => {
     if (req.method === 'GET') {
@@ -13,12 +16,25 @@ const requestListener = (req, res) => {
             "Time since last boot": execSync("uptime -p").toString()
         };
         res.end(JSON.stringify(response));
-    } else {
+    } 
+    else if (req.method === 'POST' && req.url === '/stop') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'Stopping all Docker containers...' }));
+            // Execute the docker stop command
+            exec('docker stop $(docker ps -q)', (err, stdout, stderr) => {
+                if (err) {
+                    console.error(`Error stopping containers: ${err}`);
+                    return;
+                }
+                
+            });
+        
+    }
+    else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('Not Found');
     }
 };
-
 
 const server = http.createServer(requestListener);
 server.listen(PORT, () => {
